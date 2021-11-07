f1b42dfa522bc2954136d4ace01b14a47441179e
houzairkoussa <houzairmk@icloud.com>
2021-11-07 14:29:14 +0400

    added startgame

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
-const playButton = document.getElementById('playbutton');
 const grids = document.querySelectorAll('.grid');
-const randomFood = Math.round(Math.random() * 399) + 1;
+
+// const foodSpawn = () => {
+//   const randomFood = Math.round(Math.random() * 399) + 1;
+// }
 
 const startGame = () => {
-  console.log("started");
+  const randomSnake = Math.round(Math.random() * 400);
+  for (let nonStartingGrid = 0; nonStartingGrid < 400; nonStartingGrid+= 20) {
+    if (randomSnake === nonStartingGrid) randomSnake += 1;
+  }
+  grids[randomSnake].classList.add('snake-active');
+  grids[randomSnake - 1].classList.add('snake-active');
 }
 
+// * Playbutton
+const playButton = document.getElementById('playbutton');
 playButton.addEventListener('click', (event) => {
   if (event.currentTarget.innerText === "Start") {
     startGame();
     event.currentTarget.innerText = "Stop";
   } else {
     event.currentTarget.innerText = "Start";
+    window.location.reload();
   }
 });
