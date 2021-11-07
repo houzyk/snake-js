eac2c7d407a8b15d4f56eb1abce219c9dacea72c
houzairkoussa <houzairmk@icloud.com>
2021-11-07 15:49:17 +0400

    started game start and lose function

--- js/index.js
+++ js/index.js
@@ -11,26 +11,36 @@ const startGame = () => {
   for (let nonStartingGrid = 19; nonStartingGrid <= 400; nonStartingGrid+= 20) {
     if (randomSnake === nonStartingGrid) randomSnake -= 1;
   }
+  // ! 0 FOR SNAKE HEAD, 1 FOR SNAKE BODY
   grids[randomSnake].classList.add('snake-head');
+  grids[randomSnake].dataset.game = 0;
   grids[randomSnake - 1].classList.add('snake-body');
+  grids[randomSnake - 1].dataset.game = 1;
   // * Ensures that food is not initially on snake's body
   if (initialRandomFood === randomSnake || initialRandomFood === randomSnake - 1) initialRandomFood + 2;
+  // ! 3 FOR FOOD
   grids[initialRandomFood].classList.add('food');
+  grids[initialRandomFood].dataset.game = 2;
+  return [randomSnake, randomSnake - 1];
 }
 
-const gamePlay = (key) => {
-  console.log(key)
+// TODO
+const loseGame = () => {}
+
+// TODO
+const gamePlay = (snakeInitialPosition) => {
+  const initialHeadPos = snakeInitialPosition[0];
+  const initialBodyPos = snakeInitialPosition[1];
+  window.addEventListener('keyup', (event) => {
+    console.log(event.key);
+  });
 }
 
-// TODO: Pressing Playbutton
 const playButton = document.getElementById('playbutton');
 playButton.addEventListener('click', (event) => {
   if (event.currentTarget.innerText === "Start") {
     event.currentTarget.innerText = "Stop";
-    startGame();
-    window.addEventListener('keyup', (event) => {
-      gamePlay(event.key);
-    });
+    gamePlay(startGame());
   } else {
     event.currentTarget.innerText = "Start";
     window.location.reload();
